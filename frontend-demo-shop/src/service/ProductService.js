f35f847f975b5780c00d5038a0afb63b26e7f43a
vinothkumar <net.vinothkumar@gmail.com>
2019-10-02 22:41:45 +0200

    removed comments

--- frontend-demo-shop/src/service/ProductService.js
+++ frontend-demo-shop/src/service/ProductService.js
@@ -6,28 +6,23 @@ const DEMO_SHOP_API_URL = `${BASE_URI}/demoshop`
 class ProductService {
 
     retrieveAllProducts() {
-        //console.log('executed service')
         return axios.get(`${DEMO_SHOP_API_URL}/products`);
     }
 
     retrieveProduct(id) {
-        //console.log('executed service')
         return axios.get(`${DEMO_SHOP_API_URL}/products/${id}`);
     }
 
     deleteProduct(id) {
-        //console.log('executed service')
         return axios.delete(`${DEMO_SHOP_API_URL}/products/${id}`);
     }
 
     updateProduct(id, product) {
-        //console.log('executed service')
         console.log('update product');
         return axios.put(`${DEMO_SHOP_API_URL}/products/${id}`, product);
     }
 
     createProduct(product) {
-        //console.log('executed service')
         console.log('create product');
         return axios.post(`${DEMO_SHOP_API_URL}/products/`, product);
     }
